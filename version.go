cc926a7ff679c539d3febc3e01b778718d48a78a
RockSteady <turtlecoin@gmx.com>
2020-05-23 00:07:04 -0500

    0.6.0

--- version.go
+++ version.go
@@ -4,8 +4,8 @@ package main
 func semverInfo() string {
 	var majorSemver, minorSemver, patchSemver, wholeString string
 	majorSemver = "0"
-	minorSemver = "5"
-	patchSemver = "6"
+	minorSemver = "6"
+	patchSemver = "0"
 	wholeString = majorSemver + "." + minorSemver + "." + patchSemver
 	return wholeString
 }
