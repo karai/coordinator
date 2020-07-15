a6a22f621bef45af9c5ffb6c753b97934ada5526
RockSteady <34389545+RocksteadyTC@users.noreply.github.com>
2020-07-15 18:38:30 -0500

    0.18.3
    0.18.3

--- clientops.go
+++ clientops.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net/url"
 	"strings"
@@ -16,113 +15,77 @@ type clientHeader struct {
 	ClientProtocolVersion  string `json:"client_protocol_version"`
 }
 
-func joinChannel(ktx string, pubKey, signedKey, ktxCertFileName string) *websocket.Conn {
-
-	fmt.Printf(brightcyan+"\nConnection request with ktx %s", ktx)
-
+func joinChannel(ktx, pubKey, signedKey, ktxCertFileName string, keyCollection *ED25519Keys) *websocket.Conn {
 	if isCoordinator {
 		fmt.Println(brightred + "This is for nodes running in client mode only.")
 		return nil
 	}
+	fmt.Printf(brightcyan+"\nConnection request with ktx %s", ktx)
 
 	// request a websocket connection
 	var conn = requestSocket(ktx, "1")
 
 	// using that connection, attempt to join the channel
-	var joinedChannel = sendJoinMsg(conn, pubKey, ktxCertFileName)
+	var joinedChannel = stateYourBusiness(conn, pubKey[:64])
 
 	// parse channel messages
-	socketMsgParser(ktx, pubKey, signedKey, joinedChannel)
+	socketMsgParser(ktx, pubKey, signedKey, joinedChannel, keyCollection)
 
 	// return the connection
 	return conn
 }
 
-func sendJoinMsg(conn *websocket.Conn, pubKey, ktxCertFileName string) *websocket.Conn {
-	// create join message
+func stateYourBusiness(conn *websocket.Conn, pubKey string) *websocket.Conn {
 	if isFNG {
-		joinReq := "JOIN " + pubKey
-		// Initial Connection Sends N1:PK to Coord
+		joinReq := "JOIN " + pubKey[:64]
 		_ = conn.WriteMessage(1, []byte(joinReq))
 	}
 	if !isFNG {
-		certString := readFile(ktxCertFileName)
-		rtrnReq := "RTRN " + pubKey + " " + certString
-		// Connection Sends CA:N1:S to Coord
+		certString := readFile(selfCertFilePath)
+		rtrnReq := "RTRN " + pubKey[:64] + " " + certString
 		_ = conn.WriteMessage(1, []byte(rtrnReq))
 	}
 	return conn
 }
 
 func requestSocket(ktx, protocolVersion string) *websocket.Conn {
-	// Construct a URL for the websocket.
-	// For now all that exists is the v1 API, at the /channel
-	// endpoint.
 	urlConnection := url.URL{Scheme: "ws", Host: ktx, Path: "/api/v" + protocolVersion + "/channel"}
-
-	// Announce the URL we are connecting to
-	// fmt.Printf("\nConnecting to %s", urlConnection.String())
-
-	// Make the call to the socket using
-	// the URL we composed.
 	conn, _, err := websocket.DefaultDialer.Dial(urlConnection.String(), nil)
 	handle(brightred+"There was a problem connecting to the channel: "+brightcyan, err)
 	return conn
 }
 
-func socketMsgParser(ktx, pubKey, signedKey string, conn *websocket.Conn) {
+func socketMsgParser(ktx, pubKey, signedKey string, conn *websocket.Conn, keyCollection *ED25519Keys) {
 	_, joinResponse, err := conn.ReadMessage()
-	if strings.Contains(string(joinResponse), "Welcome back") {
-		fmt.Println("\nConnected")
-	} else {
-		if len(joinResponse) != 128 {
-			fmt.Println(brightred+"\nThere seems to be a problem: ", string(joinResponse))
-			return
-		}
-		if err != nil {
-			fmt.Println(brightred+"\nThere was a problem reading this message:", err)
-			return
-		}
+	handle("There was a problem reading the socket: ", err)
+	if strings.HasPrefix(string(joinResponse), "WCBK") {
+		fmt.Printf("\nConnected to %s", ktx)
+		fmt.Printf("\nType \"send %s json-object\" to send a transaction.", ktx)
 
-		// The one issue encountered here is the sig being
-		// the wrong length, so lets make sure that is 128
-		if len(joinResponse) == 128 {
-			signature := string(bytes.TrimRight(joinResponse, "\n"))
-			err = conn.WriteMessage(1, pubkMsg)
-			_, readMessageRecvCoordPubKey, _ := conn.ReadMessage()
-			coordPubkey := string(bytes.TrimRight(readMessageRecvCoordPubKey, "\n"))
-			if verifySignedKey(pubKey, coordPubkey, signature) {
-				n1smsg := "NSIG" + signedKey
-				err = conn.WriteMessage(1, []byte(n1smsg))
-				_, n1sresponse, _ := conn.ReadMessage()
-				hashedSigCertResponse := bytes.TrimRight(n1sresponse, "\n")
-				hashedSigCertResponseNoPrefix := string(bytes.TrimLeft(hashedSigCertResponse, "CERT "))
-				if len(hashedSigCertResponseNoPrefix) == 128 {
-					var justTheDomainPartNotThePort = strings.Split(ktx, ":")
-					var ktxCertFileName = justTheDomainPartNotThePort[0] + ".cert"
-					if !fileExists(ktxCertFileName) {
-						createFile(ktxCertFileName)
-					}
-					writeFile(ktxCertFileName, hashedSigCertResponseNoPrefix)
-					fmt.Printf(brightgreen+"\n[%s] [%s] Certificate Granted\n", timeStamp(), conn.RemoteAddr())
-					fmt.Printf("file> ")
-					fmt.Printf(brightblack+"./%s", ktxCertFileName)
-					fmt.Printf(brightcyan + "\nuser> ")
-					fmt.Printf(brightblack+"%s\n", pubKey)
-					fmt.Printf(brightred + "cert> ")
-					fmt.Printf(brightblack+"%s\n"+white, hashedSigCertResponseNoPrefix)
-				} else {
-					fmt.Printf("%v is the wrong size\n%s", len(hashedSigCertResponseNoPrefix), hashedSigCertResponseNoPrefix)
-				}
-			}
-		} else {
-			fmt.Println("\nSomething is very wrong!")
-		}
+	}
+	if strings.Contains(string(joinResponse), string(capkMsg)) {
+		convertjoinResponseString := string(joinResponse)
+		trimNewLinejoinResponse := strings.TrimRight(convertjoinResponseString, "\n")
+		trimCmdPrefix := strings.TrimPrefix(trimNewLinejoinResponse, "CAPK ")
+		ncasMsgtring := signKey(keyCollection, trimCmdPrefix[:64])
+		composedNcasMsgtring := string(ncasMsg) + " " + ncasMsgtring
+		_ = conn.WriteMessage(1, []byte(composedNcasMsgtring))
+		_, certResponse, err := conn.ReadMessage()
+		convertStringcertResponse := string(certResponse) // keys := generateKeys()
+		trimNewLinecertResponse := strings.TrimRight(convertStringcertResponse, "\n")
+		trimCmdPrefixcertResponse := strings.TrimPrefix(trimNewLinecertResponse, "CERT ")
+		handle("There was an error receiving the certificate: ", err)
+		ktxCertFileName := p2pConfigDir + "/" + ktx + ".cert"
+		createFile(ktxCertFileName)
+		writeFile(ktxCertFileName, trimCmdPrefixcertResponse[:192])
+		fmt.Printf(brightgreen + "\nCert Name: ")
+		fmt.Printf(white+"%s", ktxCertFileName)
+		fmt.Printf(brightgreen + "\nCert Body: ")
+		fmt.Printf(white+"%s", trimCmdPrefixcertResponse[:192])
+		isFNG = false
 	}
 }
 
-func sendV1Transaction(channel, msg string) {
-	conn := requestSocket(channel, "1")
+func sendV1Transaction(msg string, conn *websocket.Conn) {
 	_ = conn.WriteMessage(1, []byte(msg))
-
 }
